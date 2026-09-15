b0397ad73191e50902b6225c14e7117295a7619f
Tomás Esteves <tomas.esteves@abstracta.us>
2026-09-15 17:19:47 -0300

    Revert intentional fail

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,8 +14,7 @@ def client():
 def test_health(client):
     response = client.get("/health")
     assert response.status_code == 200
-    # assert response.get_json()["status"] == "ok"
-    assert response.get_json()["status"] == "fail"
+    assert response.get_json()["status"] == "ok"
 
 
 def test_list_books_empty(client):
